d6744d48136ad84482d31673d8969eebc5523e71
Jakub Krzemiński <jakubkrzeminski08@gmail.com>
2025-12-02 19:43:36 +0100

    fix

--- Zadanie_2_Kurs_11/app/src/main/java/com/example/zadanie_2_kurs_11/MainActivity.java
+++ Zadanie_2_Kurs_11/app/src/main/java/com/example/zadanie_2_kurs_11/MainActivity.java
@@ -19,10 +19,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText fn_input = findViewById(R.id.pierwsza_liczba);
                 EditText sn_input = findViewById(R.id.druga_liczba);
-                String first_number = fn_input.getText().toString();
-                String second_number = sn_input.getText().toString();
-                int fn = Integer.parseInt(first_number);
-                int sn = Integer.parseInt(second_number);
+                int fn = Integer.parseInt(fn_input.getText().toString());
+                int sn = Integer.parseInt(sn_input.getText().toString());
                 int wynik = fn + sn;
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 intent.putExtra(ADDITION_KEY, wynik);
